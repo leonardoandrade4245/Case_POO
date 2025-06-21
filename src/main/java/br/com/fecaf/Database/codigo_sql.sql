3cb6fef57f64fc82efb8bf6afca161315a60fa5b
Leonardo Andrade <leonardo.andrade4245@gmail.com>
2025-06-20 23:18:30 -0300

    Descreva aqui o que foi alterado

--- src/main/java/br/com/fecaf/Database/codigo_sql.sql
+++ src/main/java/br/com/fecaf/Database/codigo_sql.sql
@@ -9,16 +9,46 @@ CREATE TABLE livro (
     ano_publicacao INT NOT NULL
 );
 
-insert into livro (titulo, autor, editora, ano_publicacao) values
-("Perto do Fim", "Mário Silva", "Editora Company", 2015),
-("Tempos Passados", "Sandra Oliveira", "Editora Company", 2012),
-("Avante", "Roger Andrade", "Editora Deluxe", 2005),
-("Uma Bela Vida", "Rita de Castro", "Editora Deluxe", 2001),
-("Imaginação", "Paulo Santos", "Editora Santos", 1980),
-("Uma Vingança", "Marieta Santos", "Editora Santos", 1972);
+insert into livro (titulo, autor, editora, ano_publicacao, capa) values
+("Perto do Fim", "Mário Silva", "Editora Company", 2015, "./web/img/Capa_Livro.jpg"),
+("Tempos Passados", "Sandra Oliveira", "Editora Company", 2012, "./web/img/Capa_Livro.jpg"),
+("Avante", "Roger Andrade", "Editora Deluxe", 2005, "./web/img/Capa_Livro.jpg"),
+("Uma Bela Vida", "Rita de Castro", "Editora Deluxe", 2001, "./web/img/Capa_Livro.jpg"),
+("Imaginação", "Paulo Santos", "Editora Santos", 1980, "./web/img/Capa_Livro.jpg"),
+("Uma Vingança", "Marieta Santos", "Editora Santos", 1972, "./web/img/Capa_Livro.jpg");
 
 select * from livro;
 
 ALTER TABLE livro
 ADD COLUMN capa VARCHAR(500);
 
+TRUNCATE TABLE livro;
+
+UPDATE livro
+SET capa = '/src/main/resources/templates/img/Capa_Livro.jpg';
+
+SET SQL_SAFE_UPDATES = 1;
+
+TRUNCATE TABLE livro;
+
+ALTER TABLE livro
+ADD COLUMN descricao TEXT;
+
+INSERT INTO livro (titulo, autor, editora, ano_publicacao, capa, descricao) VALUES
+("Perto do Fim", "Mário Silva", "Editora Company", 2015, "./web/img/Capa_Livro.jpg",
+"O suspense de um homem que, após perder tudo, busca redenção nos últimos dias de sua vida."),
+
+("Tempos Passados", "Sandra Oliveira", "Editora Company", 2012, "./web/img/Capa_Livro.jpg",
+"Uma emocionante viagem ao passado, onde memórias e segredos de família vêm à tona."),
+
+("Avante", "Roger Andrade", "Editora Deluxe", 2005, "./web/img/Capa_Livro.jpg",
+"Uma história de superação e coragem, onde um jovem enfrenta seus maiores medos para conquistar seus sonhos."),
+
+("Uma Bela Vida", "Rita de Castro", "Editora Deluxe", 2001, "./web/img/Capa_Livro.jpg",
+"Relato inspirador de uma mulher que, mesmo após inúmeros desafios, encontra a verdadeira felicidade."),
+
+("Imaginação", "Paulo Santos", "Editora Santos", 1980, "./web/img/Capa_Livro.jpg",
+"Um mergulho no mundo da fantasia e criatividade, onde tudo é possível e os limites não existem."),
+
+("Uma Vingança", "Marieta Santos", "Editora Santos", 1972, "./web/img/Capa_Livro.jpg",
+"Em busca de justiça, uma mulher enfrenta seu passado sombrio para acertar as contas com aqueles que a feriram.");
